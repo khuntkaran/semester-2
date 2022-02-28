6e48c70810038bb1bcd342f9949016a719f9ff40
khuntkaran <21010101108@darshan.ac.in>
2022-02-28 20:54:26 +0530

    .

--- DBMS/lab-2.sql
+++ DBMS/lab-2.sql
@@ -148,3 +148,22 @@ insert into borrow values(481,'kranti','nehru place',3000)
 
 
 /*2.3*/
+1.  delete from customers where city='bombay'
+
+2.  delete from deposit where amount<1000
+
+3.  delete from borrow where bname='ajni'
+
+4.  delete from borrow where loanno>201 and loanno<210
+
+5.  delete from deposit where adate>'1996-12-01'
+
+6.  truncate table customers 
+
+7.  delete from deposit where actno=101 and cname='anil'
+
+8.  delete from deposit where bname<>'andheri'
+
+9.  delete from borrow where amount<2000 and bname='vrce'
+
+10. drop table branch
