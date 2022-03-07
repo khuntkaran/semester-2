4016f4a25f67f37a48d640eacc1fd96de490cb3e
khuntkaran <21010101108@darshan.ac.in>
2022-03-07 18:09:54 +0530

    .

--- DBMS/lab-3.sql
+++ DBMS/lab-3.sql
@@ -1,7 +1,27 @@
-alter table student_master drop column backlog
+create table student
+( 
+	enrollment_no varchar(20),
+	name varchar(25),
+	cpi decimal(5,2),
+	birthdate datetime
+)
 
-alter table student_master alter column name varchar(35) not null
+1.  alter table student add city varchar(20),backlog int not null
 
-sp_rename 'student_master.burthdate','bdate','column'
+2.  alter table student alter column name varchar(35)
 
-alter table student_master alter column eno varchar(12)
+3.  alter table student alter column cpi int
+
+4.  sp_rename 'student.enrollment_no' , 'eno','column'
+
+5.  alter table student drop column city
+
+6.  sp_rename 'student' , 'student_master'
+
+7.  alter table student_master drop column backlog
+
+8.  alter table student_master alter column name varchar(35) not null
+
+9.  sp_rename 'student_master.birthdate','bdate','column'
+
+10. alter table student_master alter column eno varchar(12)
--- DBMS/lab-5.sql
+++ DBMS/lab-5.sql
@@ -15,15 +15,42 @@ insert into students values(1044,'bhoomi','gajera','checkyourmath.com','ahmedaba
 insert into students values(1055,'harmit','mitel',null,'rajkot','ii-bcy')
 insert into students values(1066,'ashok','jani',null,'baroda','ii-bcz')
 
-select * from students where firstname like 'k%'
+1.  select * from students where firstname like 'k%'
 
-select * from students where firstname like '_____'
+2.  select * from students where firstname like '_____'
 
-select firstname,lastname from students where city like '_____%a'
+3.  select firstname,lastname from students where city like '_____%a'
 
-select * from students where lastname like '%tel'
+4.  select * from students where lastname like '%tel'
 
-select * from students where firstname like 'ha%t'
+5.  select * from students where firstname like 'ha%t'
 
-select * from students where firstname like 'k_y%'
+6.  select * from students where firstname like 'k_y%'
 
+7.  select * from students where  website is null and firstname like '_____'
+
+8.  select * from students where firstname like '%jer'
+
+9.  select * from students where city like '[r,b]%'
+
+10. select * from students where website is not null 
+
+11. select * from students where firstname like '[a,h]%'
+
+12. select * from students where firstname like '_[a,e,i,o,u]%'
+
+13. select * from students where city like '%rod%'
+
+14. select firstname,lastname from students where website like 'bi%'
+
+15. select city from students where lastname like '______'
+
+16. select * from students where city like ''
+
+17. select * from students where division like 'ii%'
+
+18. select firstname from students where division like '%bc%'
+
+19. select stuid,city from students where division like '______' and  website is not null 
+
+20. select * from students where firstname like ''
