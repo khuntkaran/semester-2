4c779a460f3c70dac32fa6b57683d72ab7e272b3
khuntkaran <21010101108@darshan.ac.in>
2022-03-19 19:07:36 +0530

    .

---
+++ JAVA/Rndom_number.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Check {
+    int Check() {
+        Random x = new Random();
+        int number = x.nextInt(100);
+        return number;
+    }
+}
+
+class Result
+{
+    int number,i=0;
+    Result() {
+        Check c = new Check();
+         number = c.Check();
+        checknumber();
+    }
+    void checknumber()
+    {
+        System.out.print("guess number : ");
+        Scanner input = new Scanner(System.in);
+        int b = input.nextInt();
+
+        if(number>b){
+            System.out.println("number is small choose big number");
+            i++;
+            checknumber();
+        }
+        else if(number<b){
+            System.out.println("number is big choose small number");
+            i++;
+            checknumber();
+        }
+        else{
+            System.out.format("your number %d is correct\n",b);
+            System.out.format("Computer generated number is %d\n",number);
+            System.out.printf("you are %d time guess number",i+1);
+        }
+    }
+}
+
+public class Rndom_number {
+    public static void main(String[] args) {
+        Result p = new Result();
+    }
+}
