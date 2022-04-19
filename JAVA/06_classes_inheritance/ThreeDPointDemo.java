be87006cd4b771185ae050d59093df059a8a0aae
khuntkaran <21010101108@darshan.ac.in>
2022-04-19 18:11:31 +0530

    .

--- JAVA/06_classes_inheritance/ThreeDPointDemo.java
+++ JAVA/06_classes_inheritance/ThreeDPointDemo.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-class Mypoint{
+class ThreeDPoint{
     double x,y,z;
 
-    Mypoint(){
+    ThreeDPoint(){
         x=0;
         y=0;
         z=0;
     }
-    Mypoint(double x,double y,double z){
+    ThreeDPoint(double x,double y,double z){
         this.x=x;
         this.y=y;
         this.z=z;
     }
-    double distance(Mypoint a){
+    double distance(ThreeDPoint a){
         double k;
         return k= Math.sqrt(Math.pow(this.x-a.x,2)+Math.pow(this.y-a.y,2)+Math.pow(this.z-a.z,2));
     }
@@ -38,13 +38,13 @@ class Setxyz{
 public class ThreeDPointDemo{
     public static void main(String[] args){
         Setxyz ab = new Setxyz();
-        Mypoint obj1 = new Mypoint();
+        ThreeDPoint obj1 = new ThreeDPoint();
 
-        Mypoint obj2 = new Mypoint(ab.getX(), ab.gety(),ab.getz());
+        ThreeDPoint obj2 = new ThreeDPoint(ab.getX(), ab.gety(),ab.getz());
         System.out.printf("distance (%.1f,%.1f,%.1f) to (%.1f,%.1f,%.1f) : ",obj1.x,obj1.y,obj1.z,obj2.x,obj2.y,obj2.z);
         System.out.println(obj1.distance(obj2));
 
-        Mypoint obj3 = new Mypoint(ab.getX(), ab.gety(),ab.getz());
+        ThreeDPoint obj3 = new ThreeDPoint(ab.getX(), ab.gety(),ab.getz());
         System.out.printf("distance (%.1f,%.1f,%.1f) to (%.1f,%.1f,%.1f) : ",obj2.x,obj2.y,obj2.z,obj3.x,obj3.y,obj3.z);
         System.out.println(obj2.distance(obj3));
     }
