d9bcede8457f17d2024eaec5ed38e922cbb4d24a
khuntkaran <94033442+khuntkaran@users.noreply.github.com>
2022-04-25 09:24:08 +0530

    Update lab-12.sql

--- DBMS/lab-12.sql
+++ DBMS/lab-12.sql
@@ -24,3 +24,57 @@ insert into result values(103,7.6)
 insert into result values(104,8.2)
 insert into result values(105,7.0)
 insert into result values(107,8.9)
+
+
+
+
+
+create procedure insertdata2 @rno int,@name varchar(50),@branch varchar(50) 
+as
+insert into student values(@rno,@name,@branch)
+
+exec insertdata2 103,'sanjay','me'
+
+
+
+
+
+1.	create procedure alldata 
+	as 
+	select * from student inner join result on student.rno=result.rno
+	
+	exec alldata
+
+
+2.	create procedure enter_rollno @rno int
+	as
+	select * from student inner join result on student.rno=result.rno where student.rno = @rno
+
+	alter procedure enter_rollno @rno int
+	as
+	select * from student full outer join result on student.rno=result.rno where student.rno = @rno
+
+	exec enter_rollno 106
+
+
+3.	create procedure insertdata107 
+	as
+	insert into student values(107,'raj','ee')
+
+	exec insertdata107
+
+
+4.	create procedure update105
+	as
+	update student set branch='ec' where rno=105
+
+	exec update105
+
+
+5.	create procedure delete103
+	as
+	delete from student where rno=103
+
+	exec delete103
+
+select * from student
