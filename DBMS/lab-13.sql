2ed7f27172c277d966f12d05bd140f99b8c41a70
khuntkaran <21010101108@darshan.ac.in>
2022-05-02 16:51:42 +0530

    .

--- DBMS/lab-13.sql
+++ DBMS/lab-13.sql
@@ -30,57 +30,57 @@ insert into student3 values(104,'neha','ec','7.65',1)
 insert into student3 values(105,'meera','ee','5.52',2)
 insert into student3 values(106,'mahesh','ec','4.50',3)
 
-1.	create view vwPersonal1 
+1.	create view vwPersonal
 	as
 	select * from student3
 
-	select * from vwPersonal1
+	select * from vwPersonal
 
 
-2.	create view vwStudent_Details3
+2.	create view vwStudent_Details
 	as
 	select name,branch,spi from student3
 
-	select * from vwStudent_Details3
+	select * from vwStudent_Details
 
 
-3.	create view vwAcademic2
+3.	create view vwAcademic
 	as
 	select rno,name,branch from student3
 
-	select * from vwAcademic2
+	select * from vwAcademic
 
 
-4.	create view vwStudent_Data2
+4.	create view vwStudent_Data
 	as
 	select * from student3 where bklog >2
 
-	select * from vwStudent_Data2
+	select * from vwStudent_Data
 
 
-5.	create view vwStudent_Patten4
+5.	create view vwStudent_Patten
 	as
 	select rno,name,branch from student3 where name like '____'
 
-	select * from vwStudent_Patten4
+	select * from vwStudent_Patten
 
 
-6.	insert into vwAcademic2 values(107,'meet','me')
+6.	insert into vwAcademic values(107,'meet','me')
 
-7.	update vwStudent_Details3 set branch='me' where name='amit'
+7.	update vwStudent_Details set branch='me' where name='amit'
 
-8.	delete from vwAcademic2 where rno=104
+8.	delete from vwAcademic where rno=104
 
 
-9.	create view vwDisplay2
+9.	create view vwDisplay
 	as
 	select * from student3 where spi >9.5
 
-	select * from vwDisplay2
+	select * from vwDisplay
 
 
-10.	create view vwDisplay3
+10.	create view vwDisplay1
 	as
 	select * from student3  where bklog =0
 
-	select * from vwDisplay3
+	select * from vwDisplay1
