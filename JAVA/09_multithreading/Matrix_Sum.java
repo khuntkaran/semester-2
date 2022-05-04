18f849ad278bdb5894697b089811cb49c4684972
khuntkaran <21010101108@darshan.ac.in>
2022-05-04 22:05:24 +0530

    .

--- JAVA/09_multithreading/Matrix_Sum.java
+++ JAVA/09_multithreading/Matrix_Sum.java
@@ -1,16 +1,26 @@
+import java.sql.Time;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 class Matrix implements Runnable{
+	int[][] a = new int[2][2];
+	void setmatrix(){
+		Scanner input = new Scanner(System.in);
+
+		for(int i=0;i<2;i++){
+			for(int j=0;j<2;j++){
+				System.out.format("enter %d rom and %d column : ",(i+1),(j+1));
+				a[i][j]=input.nextInt();
+			}
+
+		}
+	}
 	public void run()
     {
-    	Scanner input = new Scanner(System.in);
-        int[][] a = new int[2][2];
-        for(int i=0;i<2;i++){
-        	for(int j=0;j<2;j++){
-        		System.out.format("enter %d rom and %d column : ",(i+1),(j+1));
-        		a[i][j]=input.nextInt();
-        	}
-        	
-        }
+
         for(int i=0;i<2;i++){
         	for(int j=0;j<2;j++){
         		System.out.print(a[i][j]+" ");
@@ -28,9 +38,21 @@ class Matrix implements Runnable{
 }
 
 public class Matrix_Sum {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Matrix obj = new Matrix();
+		obj.setmatrix();
+
+		Instant start = Instant.now();
+
 		Thread t1 = new Thread(obj);
 		t1.start();
+		t1.join();
+
+		Instant end = Instant.now();
+		Duration timeElapsed = Duration.between(start, end);
+		System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+
 	}
+
+
 }
