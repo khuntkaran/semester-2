cd2d5d0dbe5bbde54f5ea69a1d11e660d52b9bca
khuntkaran <21010101108@darshan.ac.in>
2022-05-12 17:23:27 +0530

    .

--- DBMS/lab-13.sql
+++ DBMS/lab-13.sql
@@ -14,22 +14,6 @@ insert into student3 values(104,'neha','ec','7.65',1)
 insert into student3 values(105,'meera','ee','5.52',2)
 insert into student3 values(106,'mahesh','ec','4.50',3)
 
-create table student3
-(
-	rno int primary key,
-	name varchar(50),
-	branch varchar(50),
-	spi decimal(4,2),
-	bklog int
-)
-
-insert into student3 values(101,'raju','ce','8.80',0)
-insert into student3 values(102,'amit','ce','2.20',3)
-insert into student3 values(103,'sanjay','me','1.50',6)
-insert into student3 values(104,'neha','ec','7.65',1)
-insert into student3 values(105,'meera','ee','5.52',2)
-insert into student3 values(106,'mahesh','ec','4.50',3)
-
 1.	create view vwPersonal
 	as
 	select * from student3
