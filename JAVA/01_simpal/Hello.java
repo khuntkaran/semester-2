5c66834d856cf140cda91ca6b6e833b06854a2db
khuntkaran <21010101108@darshan.ac.in>
2022-06-18 11:06:28 +0530

    Revert "."
    This reverts commit 67a65c01d8705536db01c9818176f957ae817023.

--- JAVA/01_simpal/Hello.java
+++ JAVA/01_simpal/Hello.java
@@ -1,4 +1,3 @@
-
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
--- JAVA/06_classes_inheritance/College.java
+++ JAVA/06_classes_inheritance/College.java
@@ -1,4 +1,3 @@
-
 class CollegeDemo{
     final String c = "CollegeDemo";
     String name = "College";
